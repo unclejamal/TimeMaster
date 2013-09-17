c1b9c1aab339878c4a5d0af8b251a45934946b8b
unclejamal <unclejamal@gmail.com>
2013-09-17 20:04:17 +0100

    added a dummy acceptance test

---
+++ src/test/java/com/pduda/jerseyjetty/GoodEveningAcceptanceTest.java
@@ -0,0 +1,10 @@
+package com.pduda.jerseyjetty;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GoodEveningAcceptanceTest {
+    @Test public void saysGoodEveningWithTheCurrentTime() {
+        assertEquals(1,1);
+    }
+}
